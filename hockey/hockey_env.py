59dbc689d0e79fea30814389a2414f1c9bce5617
Georg Martius <martius@mis.mpg.de>
2025-02-23 22:54:46 +0100

    updated set_state function

--- hockey/hockey_env.py
+++ hockey/hockey_env.py
@@ -594,15 +594,18 @@ class HockeyEnv(gym.Env, EzPickle):
   def set_state(self, state):
     """ function to revert the state of the environment to a previous state (observation)"""
     self.player1.position = (state[[0, 1]] + [CENTER_X, CENTER_Y]).tolist()
-    self.player1.angle = math.atan2(state[2], state[3])
-    self.player1.linearVelocity = [state[4], state[5]]
-    self.player1.angularVelocity = state[6]
-    self.player2.position = (state[[7, 8]] + [CENTER_X, CENTER_Y]).tolist()
-    self.player2.angle = math.atan2(state[9], state[10])
-    self.player2.linearVelocity = [state[11], state[12]]
-    self.player2.angularVelocity = state[13]
-    self.puck.position = (state[[14, 15]] + [CENTER_X, CENTER_Y]).tolist()
-    self.puck.linearVelocity = [state[16], state[17]]
+    self.player1.angle = state[2]
+    self.player1.linearVelocity = [state[3], state[4]]
+    self.player1.angularVelocity = state[5]
+    self.player2.position = (state[[6, 7]] + [CENTER_X, CENTER_Y]).tolist()
+    self.player2.angle = state[8]
+    self.player2.linearVelocity = [state[9], state[10]]
+    self.player2.angularVelocity = state[11]
+    self.puck.position = (state[[12, 13]] + [CENTER_X, CENTER_Y]).tolist()
+    self.puck.linearVelocity = [state[14], state[15]]
+    if self.keep_mode:
+        self.player1_has_puck = state[16]
+        self.player2_has_puck = state[17]
 
   def _limit_puck_speed(self):
     puck_speed = np.sqrt(self.puck.linearVelocity[0] ** 2 + self.puck.linearVelocity[1] ** 2)
